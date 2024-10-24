ff17c62834c4b95c2717e6fef985f657374ffe66
wiripol321 <johannith321@gmail.com>
2024-10-24 19:11:16 +0700

    firstpage.js

--- firstpage/src/App.js
+++ firstpage/src/App.js
@@ -4,9 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <div className="title">
       <span>Mental health game</span>
       </div>
+
+      <div class="grid-con">
+    <div class="grid-closer">1</div>
+    <div class="grid-item">2</div>
+    <div class="grid-closer">3</div>
+      </div>
+
     </div>
   );
 }
